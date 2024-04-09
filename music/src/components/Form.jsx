ba8e6813c9eaf3a3c9035a4e7585089b2644f377
abvinnovator <brahmavamsi1234@gmail.com>
2024-04-09 22:24:16 +0530

    Added Responsive Design

--- music/src/components/Form.jsx
+++ music/src/components/Form.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import music from './music.jpg';
 
 const Form = () => {
   return (
@@ -12,23 +13,21 @@ const Form = () => {
           <a href="#" className="bg-black border-2 border-red-500 text-white font-bold py-2 px-4 rounded">Home</a>
         </div>
       </div>
-      <div className="bg-black rounded-lg p-8 mt-8 flex">
-        <div className="mr-20">
-          <div className="bg-gradient-to-r from-blue-500 to-pink-500 rounded-full p-10 mb-10 ml-">
-            <svg xmlns="http://www.w3.org/2000/svg" className="h-12 w-12 text-white" viewBox="0 0 20 20" fill="currentColor">
-              <path d="M18 3a1 1 0 00-1.196-.98l-10 2A1 1 0 006 5v9.114A4.369 4.369 0 005 14c-1.658 0-3 .895-3 2s1.342 2 3 2 3-.895 3-2V7.82l8-1.6v5.894A4.37 4.37 0 0015 14c-1.658 0-3 .895-3 2s1.342 2 3 2 3-.895 3-2V3z" />
-            </svg>
+      <div className="bg-black rounded-lg p-8 mt-8 flex flex-col md:flex-row">
+        <div className="mb-8 md:mr-20 md:mb-0">
+          <div className="mb-4">
+            <img src={music} alt="Music" className="w-full h-auto rounded-lg border border-gray-500" />
           </div>
-          <div className="text-white font-bold text-lg relative">
+          <div className="text-white font-bold text-lg relative mb-4">
             Upload Music
             <div className="h-1 bg-red-500 absolute bottom-0 left-0 right-0"></div>
           </div>
-          <div className="text-white font-bold text-lg relative mt-4">
+          <div className="text-white font-bold text-lg relative mb-4">
             Upload Cover
             <div className="h-1 bg-red-500 absolute bottom-0 left-0 right-0"></div>
           </div>
         </div>
-        <div className="flex-1 ml-20">
+        <div className="flex-1">
           <form>
             <div className="mb-4">
               <input type="text" id="title" className="bg-gray-800 rounded-md p-2 w-full text-white" placeholder="Title" />
@@ -43,6 +42,7 @@ const Form = () => {
               <input type="text" id="duration" className="bg-gray-800 rounded-md p-2 w-full text-white" placeholder="Duration" />
             </div>
             <div className="mb-4">
+              <label className="text-white block mb-1">Release Date</label>
               <input type="date" id="releaseDate" className="bg-gray-800 rounded-md p-2 w-full text-white" placeholder="Proposed Release Date" />
             </div>
             <div className="mb-4">
