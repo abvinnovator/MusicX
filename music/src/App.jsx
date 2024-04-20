74e0933be5167f7d445846bdd154d7bec7f0f9e1
abvinnovator <brahmavamsi1234@gmail.com>
2024-04-20 08:14:00 +0530

    added dashboard.jsx

--- music/src/App.jsx
+++ music/src/App.jsx
@@ -6,8 +6,20 @@ import Sidebar from './sidebar/Sidebar';
 import sidebar_menu from './sidebar/sidebar_menu';
 import Home from './components/Home';
 import PaymentGateway from './components/PaymentGateway';
+import Dashboard from './Dashboard';
 
 function App() {
+  const musicData = {
+    coverImage: 'https://static.vecteezy.com/system/resources/thumbnails/000/435/728/small/1404.i033.096.S.m003.c10.Headphones_grunge.jpg',
+    musicFile: 'music_file.mp3',
+    artistName: 'John Doe',
+    title: 'Amazing Song',
+    subtitle: 'The Best Hits',
+    genre: 'Pop',
+    duration: '3:45',
+    releaseDate: '2023-04-20',
+    link: 'https://example.com/amazing-song',
+  };
   return (
     <Router>
       <div className="App flex">
@@ -19,6 +31,7 @@ function App() {
             
             <Route path="/home" element={<Home />} />
             <Route path="/paymentgateway" element={<PaymentGateway />} />
+            <Route path='/dashboard' element={<Dashboard data={musicData}/>} />
           </Routes>
         </div>
       </div>
